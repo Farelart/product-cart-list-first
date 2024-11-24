a332356434b30427fd5b13024582e6d361f66083
Farelart <ffjdganlaky@gmail.com>
2024-11-24 13:27:53 +0100

    semi last commit for deployment

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,11 +1,13 @@
+import { getImagePath } from "@/utils/paths";
+
 export const menuItems = [
   {
     id: 1,
     image: {
-      thumbnail: "/assets/images/image-waffle-thumbnail.jpg",
-      mobile: "/assets/images/image-waffle-mobile.jpg",
-      tablet: "/assets/images/image-waffle-tablet.jpg",
-      desktop: "/assets/images/image-waffle-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-waffle-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-waffle-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-waffle-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-waffle-desktop.jpg"),
     },
     name: "Waffle with Berries",
     category: "Waffle",
@@ -14,10 +16,12 @@ export const menuItems = [
   {
     id: 2,
     image: {
-      thumbnail: "/assets/images/image-creme-brulee-thumbnail.jpg",
-      mobile: "/assets/images/image-creme-brulee-mobile.jpg",
-      tablet: "/assets/images/image-creme-brulee-tablet.jpg",
-      desktop: "/assets/images/image-creme-brulee-desktop.jpg",
+      thumbnail: getImagePath(
+        "/assets/images/image-creme-brulee-thumbnail.jpg"
+      ),
+      mobile: getImagePath("/assets/images/image-creme-brulee-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-creme-brulee-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-creme-brulee-desktop.jpg"),
     },
     name: "Vanilla Bean Crème Brûlée",
     category: "Crème Brûlée",
@@ -26,10 +30,10 @@ export const menuItems = [
   {
     id: 3,
     image: {
-      thumbnail: "/assets/images/image-macaron-thumbnail.jpg",
-      mobile: "/assets/images/image-macaron-mobile.jpg",
-      tablet: "/assets/images/image-macaron-tablet.jpg",
-      desktop: "/assets/images/image-macaron-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-macaron-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-macaron-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-macaron-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-macaron-desktop.jpg"),
     },
     name: "Macaron Mix of Five",
     category: "Macaron",
@@ -38,10 +42,10 @@ export const menuItems = [
   {
     id: 4,
     image: {
-      thumbnail: "/assets/images/image-tiramisu-thumbnail.jpg",
-      mobile: "/assets/images/image-tiramisu-mobile.jpg",
-      tablet: "/assets/images/image-tiramisu-tablet.jpg",
-      desktop: "/assets/images/image-tiramisu-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-tiramisu-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-tiramisu-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-tiramisu-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-tiramisu-desktop.jpg"),
     },
     name: "Classic Tiramisu",
     category: "Tiramisu",
@@ -50,10 +54,10 @@ export const menuItems = [
   {
     id: 5,
     image: {
-      thumbnail: "/assets/images/image-baklava-thumbnail.jpg",
-      mobile: "/assets/images/image-baklava-mobile.jpg",
-      tablet: "/assets/images/image-baklava-tablet.jpg",
-      desktop: "/assets/images/image-baklava-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-baklava-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-baklava-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-baklava-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-baklava-desktop.jpg"),
     },
     name: "Pistachio Baklava",
     category: "Baklava",
@@ -62,10 +66,10 @@ export const menuItems = [
   {
     id: 6,
     image: {
-      thumbnail: "/assets/images/image-meringue-thumbnail.jpg",
-      mobile: "/assets/images/image-meringue-mobile.jpg",
-      tablet: "/assets/images/image-meringue-tablet.jpg",
-      desktop: "/assets/images/image-meringue-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-meringue-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-meringue-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-meringue-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-meringue-desktop.jpg"),
     },
     name: "Lemon Meringue Pie",
     category: "Pie",
@@ -74,10 +78,10 @@ export const menuItems = [
   {
     id: 7,
     image: {
-      thumbnail: "/assets/images/image-cake-thumbnail.jpg",
-      mobile: "/assets/images/image-cake-mobile.jpg",
-      tablet: "/assets/images/image-cake-tablet.jpg",
-      desktop: "/assets/images/image-cake-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-cake-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-cake-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-cake-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-cake-desktop.jpg"),
     },
     name: "Red Velvet Cake",
     category: "Cake",
@@ -86,10 +90,10 @@ export const menuItems = [
   {
     id: 8,
     image: {
-      thumbnail: "/assets/images/image-brownie-thumbnail.jpg",
-      mobile: "/assets/images/image-brownie-mobile.jpg",
-      tablet: "/assets/images/image-brownie-tablet.jpg",
-      desktop: "/assets/images/image-brownie-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-brownie-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-brownie-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-brownie-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-brownie-desktop.jpg"),
     },
     name: "Salted Caramel Brownie",
     category: "Brownie",
@@ -98,10 +102,10 @@ export const menuItems = [
   {
     id: 9,
     image: {
-      thumbnail: "/assets/images/image-panna-cotta-thumbnail.jpg",
-      mobile: "/assets/images/image-panna-cotta-mobile.jpg",
-      tablet: "/assets/images/image-panna-cotta-tablet.jpg",
-      desktop: "/assets/images/image-panna-cotta-desktop.jpg",
+      thumbnail: getImagePath("/assets/images/image-panna-cotta-thumbnail.jpg"),
+      mobile: getImagePath("/assets/images/image-panna-cotta-mobile.jpg"),
+      tablet: getImagePath("/assets/images/image-panna-cotta-tablet.jpg"),
+      desktop: getImagePath("/assets/images/image-panna-cotta-desktop.jpg"),
     },
     name: "Vanilla Panna Cotta",
     category: "Panna Cotta",
